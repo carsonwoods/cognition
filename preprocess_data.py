7651d9e356d6eb88a7f6d98a2d2eb5ed9693d2a5
Carson Woods <cwoods289@gmail.com>
2018-11-03 08:23:57 -0500

    Script for preprocessing data

---
+++ preprocess_data.py
@@ -0,0 +1,18 @@
+"""
+Cleans and preprocesses data for fitting to CNN
+"""
+
+import os, sys
+from PIL import Image
+
+size = 150, 150
+
+for filename in os.listdir("data/validation/Carson_Woods/"):
+    outfile = os.getcwd() + "/data/validation/Carson_Woods/" + filename
+    print(outfile)
+    try:
+        im = Image.open("./data/validation/Carson_Woods/" + filename)
+        im.thumbnail(size, Image.ANTIALIAS)
+        im.save(outfile, "JPEG")
+    except IOError:
+        print("Cannot resize '%s'" % filename)

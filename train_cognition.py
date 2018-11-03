fa962e774dbcb07a292acdff0a42021dec2ce34e
Carson Woods <cwoods289@gmail.com>
2018-11-03 08:24:38 -0500

    Script for training cognition network

--- train_cognition.py
+++ train_cognition.py
@@ -1,14 +1,91 @@
-import os, sys
-from PIL import Image
+import numpy as np
+import os.path
 
-size = 64, 64
+# Sets random seed to constant for reproducibility
+np.random.seed(123)
 
-for filename in os.listdir("training_data"):
-    outfile = os.getcwd() + "/training_data/preprocessed/" + filename + ".jpg"
-    print(outfile)
-    try:
-        im = Image.open("./training_data/" + filename)
-        im.thumbnail(size, Image.ANTIALIAS)
-        im.save(outfile, "JPEG")
-    except IOError:
-        print("Cannot resize '%s'" % filename)
+from keras import backend as K
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Convolution2D, MaxPooling2D, Conv2D
+from keras.utils import np_utils
+from keras.preprocessing import image
+from keras.preprocessing.image import ImageDataGenerator
+
+# Set Data Classification/Training Details
+num_classes = 1
+batch_size = 16
+epoch_count = 10
+nb_train_samples = 9000
+nb_validation_samples = 960
+train_data_dir = 'data/train'
+validation_data_dir = 'data/validation'
+
+# Set Image Data size
+img_width, img_height = 150, 150
+
+if K.image_data_format() == 'channels_first':
+    input_shape = (3, img_width, img_height)
+else:
+    input_shape = (img_width, img_height, 3)
+
+# Begin creating model to train against for facial recognition
+model = Sequential()
+model.add(Conv2D(32, (3, 3), input_shape=input_shape))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+# Add two fully connected layers
+model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer='rmsprop',
+              metrics=['accuracy'])
+
+# Augment data for training. Due to limited dataset this helps generate more random
+# data to use.
+train_datagen = ImageDataGenerator(
+    rescale=1. / 255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True)
+
+# Minor augmentation on testing data. Less is needed because we need to account
+# for less possibilities in testing. Only rescales.
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+
+train_generator = train_datagen.flow_from_directory(
+    train_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='binary')
+
+validation_generator = test_datagen.flow_from_directory(
+    validation_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='binary')
+
+
+model.fit_generator(
+    train_generator,
+    steps_per_epoch=nb_train_samples // batch_size,
+    epochs=epoch_count,
+    validation_data=validation_generator,
+    validation_steps=nb_validation_samples // batch_size)
+
+model.save('first_try.h5')
